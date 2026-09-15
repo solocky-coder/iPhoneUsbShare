65615ff4136cdb2556631e7a77e97100215070e0
PAINT711 <solocky@gmail.com>
2026-09-15 17:40:46 +0200

    Remove temporary composite probe

--- src/CompositeDriverProbe.cs
+++
@@ -1,7 +0,0 @@
-namespace iPhoneUsbShare;
-
-// Placeholder diagnostic marker for the composite-parent repair work.
-internal static class CompositeDriverProbe
-{
-    internal const string ExpectedParentService = "usbccgp";
-}
